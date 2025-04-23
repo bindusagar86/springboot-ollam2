2b77e699d1541f5d1d0fe43d3c843c190ec28648
bindu <bindusagarbhoi@gmail.com>
2025-04-23 11:50:53 +0530

    initial commit

---
+++ src/main/java/com/bbtech/SpringbootOllam2Application.java
@@ -0,0 +1,13 @@
+package com.bbtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOllam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOllam2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbtech/SpringbootOllam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOllam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
